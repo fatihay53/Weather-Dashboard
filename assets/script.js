1ec5a42485d1385a46b44dec6d343ee080e81dec
Fatih Ay <Fatih Ay>
2021-02-02 20:19:02 -0500

    API link fixed and search bug solved

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,66 @@
 
-var city
-var cities = []
-var nameCity =  document.getElementById('cityName')
 var apiur1 
 var apiur2
  var weekWeather
  var weekWeather2
+var name2
+var cityBtn
+window.onload = function () {
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+
+    for (let index = 0; index < history.length; index ++ ) {
+        let li = `<lis class='list-group-item list-group-item-action>${history[index]}</li>`;
+        list.innerHTML += li;
+    }
+};
 
 
+document.querySelector('#searchButton').onclick = function () {
+    let searchValue = document.querySelector('#inputValue').value;
+    if (searchValue) {
+        document.querySelector('#inputValue').value = "" ;
+        getWeather(searchValue);
+    }
+}
+
+let list = document.querySelector('.history');
+list.addEventListener('click', function(event) {
+    if (!event.target.classList.contains('history')) {
+        getWeather(event.target.textContent);
+    }
+});
+function makeRow (text) {
+    let li = `<lis class='list-group-item list-group-item-action'>${text}</li> `
+    list.innerHTML += li;
+}
  
-async function getWeather(){
-    var city = document.getElementById('inputValue').value
-    apiur1 =`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=166a433c57516f51dfab1f7edaed8413&units=imperial`
+async function getWeather(searchValue){
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+    if (history.indexOf(searchValue) === -1){
+        history.push(searchValue);
+
+        localStorage.setItem('history', JSON.stringify(history));
+
+        makeRow(searchValue);
+    }
+    
+    apiur1 =`https://api.openweathermap.org/data/2.5/forecast/daily?q=${searchValue}&appid=166a433c57516f51dfab1f7edaed8413&units=imperial`
     
     // get the city information
+  
     weekWeather = await fetch( apiur1 ).then( r=>r.json() )
-    document.getElementById('cityName').textContent = weekWeather.city.name + ' ' + `${moment().format('L')}`  
+    name2 = weekWeather.city.name
+    document.getElementById('cityName').textContent = name2 + ' ' + `${moment().format('L')}`  
     var windFix = Math.floor(`${weekWeather.list[0].speed}`);
     document.getElementById('windSpeed').innerHTML = `WindSpeed: ${windFix} MPH`
-    // cityName button after clicked search button
-    const cityName = document.createElement('button')
-    cityName.setAttribute('class',  `btn-secondary ${city.name}`)
-    cityName.setAttribute('id', 'deleted-when-cleared')
-    cityName.textContent = `${weekWeather.city.name}`
-    document.querySelector('.list-group').appendChild(cityName)
-    document.getElementById(`${weekWeather.city.name}`).onclick = function () {testing(this.id)}
-
-    
-    cities.push(`${weekWeather.city.name}`)
-    localStorage.setItem('cities', JSON.stringify(cities));
-    
-    
-    
+                    
     for ( i=1;i<7; i++){
         var element = document.getElementById('col'); 
         element.classList.remove('hidden');
         document.getElementById(`humidity${i}`).innerHTML = `Humidity: ${weekWeather.list[i-1].humidity} %`
         var temperatureFix = Math.floor(`${weekWeather.list[i-1].temp.day}`)
         document.getElementById(`temperature${i}`).innerHTML = `Temp: ${temperatureFix} °C`
-        document.querySelector(`#imageWeather${i}`).src = `http://openweathermap.org/img/wn/${weekWeather.list[i-1].weather[0].icon}@2x.png`
+        document.querySelector(`#imageWeather${i}`).src = `https://openweathermap.org/img/wn/${weekWeather.list[i-1].weather[0].icon}@2x.png`
         //background image depends of weather condition
         if (`${weekWeather.list[i-1].weather[0].main}` == "Snow" ){
             document.querySelector(`#card${i-1}`).style.backgroundImage = "url(assets/snow.gif)";
@@ -53,7 +75,7 @@ async function getWeather(){
             document.getElementById(`card${i-1}`).style.backgroundImage = "url(assets/rain.gif)" 
         else{  }
 
-        document.querySelector(`#cardtime${i}`).textContent =  moment().add(`${i}`, 'day').format('L')
+        document.querySelector(`#cardtime${i}`).innerHTML =  moment().add(`${i}`, 'day').format('L')
         var cardwitdh = document.querySelector(`#card${i}`)
         cardwitdh.style.margin = "5px"
         cardwitdh.style.width = "165px"
@@ -61,24 +83,17 @@ async function getWeather(){
         var coordinateLat = weekWeather.city.coord.lat
         var coordinateLon = weekWeather.city.coord.lon
     
-        apiur2 =`http://api.openweathermap.org/data/2.5/uvi?lat=${coordinateLat}&lon=${coordinateLon}&appid=166a433c57516f51dfab1f7edaed8413` 
+        apiur2 =`https://api.openweathermap.org/data/2.5/uvi?lat=${coordinateLat}&lon=${coordinateLon}&appid=166a433c57516f51dfab1f7edaed8413` 
         weekWeather2 = await fetch( apiur2 ).then( r=>r.json() )
     
         document.getElementById(`uvIndex`).innerHTML = `UV Index: ${weekWeather2.value }`
-        
-
-
-
-
-//         if (localStorage.getItem("cities"))  {
-//             var citiesObject = localStorage.getItem('cities');
-//             cities = JSON.parse(citiesObject)
-//             for (var i =0; i<citiesObject.length; i++ )
-//             {const cityName = document.createElement('button')
-//             cityName.setAttribute('class',  `btn-secondary ${city.name}`)
-//             cityName.setAttribute('id', 'deleted-when-cleared')
-//             cityName.textContent = `${weekWeather.city.name}`} 
-//         }
-
+    //   
+    }
+    
 }
+function testing (clicked_id){
+    city = clicked_id
+    getWeather()
 }
+
+//  document.querySelector('#deleted-when-cleared').addEventListener('click',getWeather(citylist));
